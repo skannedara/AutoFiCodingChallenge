47f9eb32a92d82b98a7d20a8ff34a8030d69bda8
Siva Kumar Kannedara <sk_kannedara@yahoo.co.in>
2021-03-10 22:39:57 -0600

    Simple API code to access CSV format code

---
+++ app.js
@@ -0,0 +1,41 @@
+'use strict';
+
+const http = require('http');
+const fs = require('fs');
+
+const express = require('express');
+const multer = require('multer');
+const csv = require('fast-csv');
+
+const Router = express.Router;
+const upload = multer({ dest: 'tmp/csv/' });
+const app = express();
+const router = new Router();
+const server = http.createServer(app);
+const port = 9000
+
+router.post('/', upload.single('file'), function (req, res) {
+  const fileRows = [];
+
+  // open uploaded file
+  csv.fromPath(req.file.path)
+    .on("data", function (data) {
+      fileRows.push(data); // push each row
+    })
+    .on("end", function () {
+      console.log(fileRows)
+      fs.unlinkSync(req.file.path);   // remove temp file
+      //process "fileRows" and respond
+    })
+});
+
+app.use('/upload-csv', router);
+
+// Start server
+function startServer() {
+  server.listen(port, function () {
+    console.log('Express server listening on ', port);
+  });
+}
+
+setImmediate(startServer);
